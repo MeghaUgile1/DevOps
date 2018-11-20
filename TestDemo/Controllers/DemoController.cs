0921580c82898d3a3edb8763ff5a835854bb6b78
Megha Rajendra Ugile (Semiconductor, Computing & Storage (SCS)) <ME381404@wipro.com>
2018-11-20 16:23:55 +0530

    Commited Demo Controller

---
+++ TestDemo/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public string Index()
+        {
+            return "Hello Demo";
+        }
+    }
+}
