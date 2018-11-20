89b3fe5a82fd5b5f6a9b52c55b5a4dedd70777fb
Megha Rajendra Ugile (Semiconductor, Computing & Storage (SCS)) <ME381404@wipro.com>
2018-11-20 17:17:11 +0530

    Comitted Changes

---
+++ TestDemo/Controllers/VSTSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDemo.Controllers
+{
+    public class VSTSController : Controller
+    {
+        // GET: VSTS
+        public string Index()
+        {
+            return "VSTS Hello Controller";
+        }
+    }
+}
