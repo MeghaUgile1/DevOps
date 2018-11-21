061eb9b8c6fa375998d464481cec966412dc04fc
Megha Rajendra Ugile (Semiconductor, Computing & Storage (SCS)) <ME381404@wipro.com>
2018-11-21 10:10:11 +0530

    Comitted Test1

---
+++ TestDemo/Controllers/Test1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestDemo.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public string Index()
+        {
+            return "Test1 Controller";
+        }
+    }
+}
